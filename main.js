8f191e12c4d901a3c0180293e0e71663ca2c6e28
Manh1234-ne <manhnon16@gmail.com>
2026-01-06 15:24:12 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,11 @@
+// String
+var myName = "manhmo";
+console.log(myName);
+// Number
+var myAge = 20;
+var score = 9.5;
+var match = 10;
+console.log(myAge);
+// Boolean
+var myGender = true;
+console.log(myGender);
---
+++ main.ts
@@ -0,0 +1,14 @@
+// String
+let myName: string = "manhmo";
+console.log(myName);
+
+// Number
+let myAge: number = 20;
+let score: number = 9.5;
+let match = 10;
+console.log(myAge);
+
+
+// Boolean
+let myGender: boolean = true;
+console.log(myGender);
