f789973d2680de9f19eb1cf4bf42042bfd1d86fd
Manh1234-ne <manhnon16@gmail.com>
2026-01-08 09:30:43 +0700

    lession01

--- main.js
+++ main.js
@@ -1,11 +1,35 @@
-// String
-var myName = "manhmo";
+// string;
+var myName = 'chinhpd5';
 console.log(myName);
-// Number
-var myAge = 20;
+// number
+var myAge = 10;
 var score = 9.5;
-var match = 10;
-console.log(myAge);
-// Boolean
+var math = -10;
+// boolean
 var myGender = true;
-console.log(myGender);
+// array
+var myArray = ["nam", "Long", "bình"];
+// myArray.push(10);
+var myArray2 = [0, 1, -3, 4.5];
+for (var i = 0; i < myArray.length; i++) {
+    console.log(myArray[i]);
+}
+// object
+var myInfo = {
+    name: 'chinhpd5',
+    age: 10,
+    gender: false
+};
+console.log("T\u00F4i t\u00EAn l\u00E0 ".concat(myInfo.name, ", \n  Tu\u1ED5i: ").concat(myInfo.age, ", \n  Gi\u1EDBi t\u00EDnh: ").concat(myInfo.gender ? 'Nam' : 'Nữ'));
+function showInfo(name) {
+    return "Xin ch\u00E0o ".concat(name);
+}
+console.log(showInfo('chinhpd5'));
+var sum = function (a, b) {
+    return a + b;
+};
+console.log(sum(1, 2));
+var myFun = function () {
+    console.log("Hello");
+};
+myFun();
--- main.ts
+++ main.ts
@@ -1,14 +1,54 @@
-// String
-let myName: string = "manhmo";
+// string;
+let myName: string = 'chinhpd5';
 console.log(myName);
 
-// Number
-let myAge: number = 20;
+// number
+let myAge: number = 10;
 let score: number = 9.5;
-let match = 10;
-console.log(myAge);
+let math = -10;
 
-
-// Boolean
+// boolean
 let myGender: boolean = true;
-console.log(myGender);
+
+// array
+let myArray: string[] = ["nam", "Long", "bình"];
+// myArray.push(10);
+let myArray2: number[] = [0, 1, -3, 4.5];
+
+for(let i: number = 0; i< myArray.length; i++){
+  console.log(myArray[i]);
+}
+
+// object
+let myInfo: {
+  name: string,
+  age: number,
+  gender: boolean
+} = {
+  name: 'chinhpd5',
+  age: 10,
+  gender: false
+}
+
+console.log(
+  `Tôi tên là ${myInfo.name}, 
+  Tuổi: ${myInfo.age}, 
+  Giới tính: ${myInfo.gender ? 'Nam': 'Nữ'}`
+);
+
+function showInfo(name: string): string {
+  return `Xin chào ${name}`
+}
+
+console.log(showInfo('chinhpd5'));
+
+const sum = (a: number, b: number): number => {
+  return a+b;
+}
+console.log(sum(1, 2));
+
+const myFun = (): void => {
+  console.log("Hello");
+  
+}
+myFun()
